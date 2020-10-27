25432756f8ba0e5e576badf0fa51673049f53e9b
Jacky <jacky2wong@gmail.com>
2020-10-27 20:57:34 +1100

    Remove hard-coded dependencies

--- vectorhub/import_utils.py
+++ vectorhub/import_utils.py
@@ -9,26 +9,8 @@ from pkg_resources import resource_filename
 from importlib import import_module, invalidate_caches
 
 
-def get_package_requirements(requirement_type):
-    requirements = {
-        "numpy": ["core"],
-        "requests": ["core"],
-        "pytest": ["test"],
-        "sphinx-rtd-theme>=0.5.0": ["test"],
-        "imageio": ["encoders-image-tfhub"],
-        "soundfile": ["encoders-audio-tfhub"],
-        "librosa": ["audio-encoder", "encoders-audio-tfhub"],
-        "tensorflow": ["encoders-text-tfhub", "encoders-audio-tfhub", "encoders-image-tfhub", "encoders-text-tf-transformers", "encoders-text-tfhub-windows"],
-        "tensorflow_hub": ["encoders-text-tfhub", "encoders-audio-tfhub", "encoders-image-tfhub", "encoders-text-tfhub-windows"],
-        "tensorflow_text": ["encoders-text-tfhub"],
-        "tf-models-official": ["encoders-text-tfhub", "encoders-text-tfhub-windows"],
-        "bert-for-tf2": ["encoders-text-tfhub", "encoders-text-tfhub-windows"],
-        "torch": ["encoders-audio-pytorch", "encoders-text-torch-transformers"],
-        "fairseq": ["encoders-audio-pytorch"],
-        "transformers": ["encoders-text-torch-transformers", "encoders-text-tf-transformers"],
-        "moviepy": ["encoders-video"],
-        "opencv-python": ["encoders-video"]
-    }
+def get_package_requirements(requirements_fn=resource_filename('vectorhub', 'extra_requirements.json')):
+    requirements = json.load(open(requirements_fn, 'r'))
     dependencies = []
     for k, v in requirements.items():
         if requirement_type in v:
