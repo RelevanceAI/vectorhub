82f97b50144f9e92a4c921219d859f8da06deaf2
Jacky <jacky2wong@gmail.com>
2020-10-28 00:13:28 +1100

    Add hard-coded dependencies because of absence of extra_requirements.json

--- vectorhub/import_utils.py
+++ vectorhub/import_utils.py
@@ -5,7 +5,6 @@ import sys
 import warnings
 import pkg_resources
 import json
-from pkg_resources import resource_filename, resource_exists
 from importlib import import_module, invalidate_caches
 
 
@@ -13,13 +12,25 @@ def get_package_requirements(requirement_type: str):
     """
         Load in extra_requirements.json from the package
     """
-    if resource_exists('vectorhub', 'extra_requirements.json'):
-        requirements_fn = resource_filename('vectorhub', 'extra_requirements.json')
-        requirements = json.load(open(requirements_fn, 'r'))
-    else:
-        requirements_fn = resource_filename('vectorhub', '../extra_requirements.json')
-        requirements = json.load(open(requirements_fn))
-    
+    requirements = {
+        "numpy": ["core"],	
+        "requests": ["core"],	
+        "pytest": ["test"],	
+        "sphinx-rtd-theme>=0.5.0": ["test"],	
+        "imageio": ["encoders-image-tfhub"],	
+        "soundfile": ["encoders-audio-tfhub"],	
+        "librosa": ["audio-encoder", "encoders-audio-tfhub"],	
+        "tensorflow": ["encoders-text-tfhub", "encoders-audio-tfhub", "encoders-image-tfhub", "encoders-text-tf-transformers", "encoders-text-tfhub-windows"],	
+        "tensorflow_hub": ["encoders-text-tfhub", "encoders-audio-tfhub", "encoders-image-tfhub", "encoders-text-tfhub-windows"],	
+        "tensorflow_text": ["encoders-text-tfhub"],	
+        "tf-models-official": ["encoders-text-tfhub", "encoders-text-tfhub-windows"],	
+        "bert-for-tf2": ["encoders-text-tfhub", "encoders-text-tfhub-windows"],	
+        "torch": ["encoders-audio-pytorch", "encoders-text-torch-transformers"],	
+        "fairseq": ["encoders-audio-pytorch"],	
+        "transformers": ["encoders-text-torch-transformers", "encoders-text-tf-transformers"],	
+        "moviepy": ["encoders-video"],	
+        "opencv-python": ["encoders-video"]	
+    }
     dependencies = []
     for k, v in requirements.items():
         if requirement_type in v:
