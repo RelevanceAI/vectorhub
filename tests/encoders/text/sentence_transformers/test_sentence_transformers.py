d5c3133db86c1299cd466a02f5e7de454f48c498
Jacky <jacky2wong@gmail.com>
2020-11-03 00:36:02 +1100

    Add bulk encoding test for sentence transformers

--- tests/encoders/text/sentence_transformers/test_sentence_transformers.py
+++ tests/encoders/text/sentence_transformers/test_sentence_transformers.py
@@ -8,4 +8,4 @@ def test_sentence_transformers():
     for k, v in LIST_OF_URLS.items():
         enc = SentenceTransformer2Vec(model_name=k)
         assert len(enc.encode("Let us go to the beach today.")) == v['vector_length']
-    
+        assert len(enc.bulk_encode(["hi", "whats up"])) == 2
