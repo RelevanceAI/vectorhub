42adf390a246c9efed5fc0ec869ea63583e464ce
Jacky <jacky2wong@gmail.com>
2020-11-03 00:27:30 +1100

    Make list of urls global

--- vectorhub/encoders/text/sentence_transformers/sentence_transformers.py
+++ vectorhub/encoders/text/sentence_transformers/sentence_transformers.py
@@ -9,6 +9,7 @@ if is_all_dependency_installed(MODEL_REQUIREMENTS['encoders-text-sentence-transf
 
 
 SentenceTransformerModelDefinition = ModelDefinition(
+    model_id='text/sentence-transformers',
     model_name="Sentence Transformer Models", 
     vector_length='Depends on model.', 
     description="These are Sentence Transformer models from sbert.net by UKPLab.",
@@ -23,42 +24,50 @@ SentenceTransformerModelDefinition = ModelDefinition(
     """
 )
 
+LIST_OF_URLS = {
+    'distilroberta-base-paraphrase-v1' : {"vector_length": 768},
+    'xlm-r-distilroberta-base-paraphrase-v1' : {"vector_length": 768},
+
+    'distilbert-base-nli-stsb-mean-tokens' : {"vector_length": 768},
+    'bert-large-nli-stsb-mean-tokens' : {"vector_length": 1024},
+    'roberta-base-nli-stsb-mean-tokens' : {"vector_length": 768},
+    'roberta-large-nli-stsb-mean-tokens' : {"vector_length": 1024},
+
+    'distilbert-base-nli-stsb-quora-ranking' : {"vector_length": 768},
+    'distilbert-multilingual-nli-stsb-quora-ranking' : {"vector_length": 768},
+
+    'distilroberta-base-msmarco-v1' : {"vector_length": 768},
+
+    'distiluse-base-multilingual-cased-v2' : {"vector_length": 512},
+    'xlm-r-bert-base-nli-stsb-mean-tokens' : {"vector_length": 768},
+
+    'bert-base-wikipedia-sections-mean-tokens' : {"vector_length": 768},
+
+    'LaBSE' : {"vector_length": 768},
+
+    'average_word_embeddings_glove.6B.300d' : {"vector_length": 300},
+    'average_word_embeddings_komninos' : {"vector_length": 300},
+    'average_word_embeddings_levy_dependency' : {"vector_length": 768},
+    'average_word_embeddings_glove.840B.300d' : {"vector_length": 300}
+}
+
 __doc__ = SentenceTransformerModelDefinition.create_docs()
 
 
 class SentenceTransformer2Vec(BaseText2Vec):
     definition = SentenceTransformerModelDefinition
     def __init__(self, model_name: str, config=None):
-        list_of_urls = {
-            'distilroberta-base-paraphrase-v1' : {"vector_length": 768},
-            'xlm-r-distilroberta-base-paraphrase-v1' : {"vector_length": 768},
-
-            'distilbert-base-nli-stsb-mean-tokens' : {"vector_length": 768},
-            'bert-large-nli-stsb-mean-tokens' : {"vector_length": 1024},
-            'roberta-base-nli-stsb-mean-tokens' : {"vector_length": 768},
-            'roberta-large-nli-stsb-mean-tokens' : {"vector_length": 1024},
-
-            'distilbert-base-nli-stsb-quora-ranking' : {"vector_length": 768},
-            'distilbert-multilingual-nli-stsb-quora-ranking' : {"vector_length": 768},
-
-            'distilroberta-base-msmarco-v1' : {"vector_length": 768},
-
-            'distiluse-base-multilingual-cased-v2' : {"vector_length": 512},
-            'xlm-r-bert-base-nli-stsb-mean-tokens' : {"vector_length": 768},
-
-            'bert-base-wikipedia-sections-mean-tokens' : {"vector_length": 768},
-
-            'LaBSE' : {"vector_length": 768},
-
-            'average_word_embeddings_glove.6B.300d' : {"vector_length": 300},
-            'average_word_embeddings_komninos' : {"vector_length": 300},
-            'average_word_embeddings_levy_dependency' : {"vector_length": 768},
-            'average_word_embeddings_glove.840B.300d' : {"vector_length": 300}
-        }
-        self.validate_model_url(model_url, list_of_urls)
-        self.vector_length = list_of_urls[model_url]["vector_length"]
+        self.list_of_urls = LIST_OF_URLS
+        self.validate_model_url(model_url, LIST_OF_URLS)
+        self.vector_length = LIST_OF_URLS[model_url]["vector_length"]
         self.model = SentenceTransformer(model_url)
-    
+
+    def get_list_of_urls(self):
+        """
+            Return list of URLS.
+        """
+        return self.list_of_urls
+
     @catch_vector_errors
     def encode(self, text: str) -> List[float]:
         """
