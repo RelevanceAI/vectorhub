3cadcb89d074f2dc9631bc5a2765bee0d18d0613
Jacky <jacky2wong@gmail.com>
2020-11-10 00:11:40 +1100

    Update requirements and fix template markdown

---
+++ utils/template.md
@@ -0,0 +1,28 @@
+---
+model_id: 'Must be unique - check AutoEncoder'
+model_name: "Fill in"
+vector_length: 'Fill in here.'
+paper: Preferably link to arxiv paper goes here.
+repo: Repository link.
+release_date: date format is 4 digit year - 2 digit month - 2 digit day
+installation: pip install vectorhub[vectorhub_version]
+---
+
+The base template for the markdown file goes here.
+
+## Description
+
+We have a splitter that splits on the hashtags for each subsection. This creates a field called model_description.
+
+## Limitations
+
+This creates a field called limitations. The value in this field goes in the contents below it.
+
+## Other Notes
+
+This creates a field called other_notes. 
+
+
+## Example
+
+Note: Keep this at the bottom to ensure UI consistency on vectorhub. This creates a field called example.
--- utils/upload_cards.py
+++ utils/upload_cards.py
@@ -15,12 +15,34 @@ if __name__=="__main__":
     args = parser.parse_args()
     
     docs =  get_model_definitions(None)
+
+    # Generate 1 sentence summaries for the models
+    from transformers import PegasusTokenizer, PegasusForConditionalGeneration
+    from typing import List
+    mname = "google/pegasus-xsum"
+
+    model = PegasusForConditionalGeneration.from_pretrained(mname)
+    tok = PegasusTokenizer.from_pretrained(mname)
+    # batch = tok.prepare_seq2seq_batch(src_texts=[PGE_ARTICLE])  # don't need tgt_text for inference
+    # gen = model.generate(**batch)  # for forward pass: model(**batch)
+    # summary: List[str] = tok.batch_decode(gen, skip_special_tokens=True)
+
+    def summarise(text):
+        batch = tok.prepare_seq2seq_batch(src_texts=[text])  # don't need tgt_text for inference
+        gen = model.generate(**batch)
+        return tok.batch_decode(gen, skip_special_tokens=True)[0]
+    
+    for doc in docs:
+        short_description = summarise(doc['description'])
+        docs[0]['short_description'] = short_description
+
     vi_client = ViClient(os.environ['VH_USERNAME'], os.environ['VH_API_KEY'])
     if args.collection_name in vi_client.list_collections():
         vi_client.delete_collection(args.collection_name)
         time.sleep(5)
     text_encoder = ViText2Vec(os.environ['VH_USERNAME'], os.environ['VH_API_KEY'])
-    vi_client.insert_documents(args.collection_name, docs)
+    vi_client.insert_documents(args.collection_name, docs, models={'description': text_encoder})
     print("Checking Documents:")
-    print(vi_client.head('vh_markdown_2'))
-    print(vi_client.collection_schema('vh_markdown_2'))
+    print(vi_client.head(args.collection_name))
+    print(vi_client.head(args.collection_name)['vector_length'])
+    print(vi_client.collection_schema(args.collection_name))
