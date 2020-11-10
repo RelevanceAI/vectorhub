b8ae7d994480ae87c440b1810ab59ee0a988bdce
Jacky <jacky2wong@gmail.com>
2020-11-11 00:41:51 +1100

    Fix sentence transformers

--- vectorhub/encoders/text/sentence_transformers/sentence_auto_transformers.py
+++ vectorhub/encoders/text/sentence_transformers/sentence_auto_transformers.py
@@ -1,14 +1,16 @@
 from typing import List
-from ..base import BaseText2Vec
+from datetime import date
 from ....base import catch_vector_errors
 from ....doc_utils import ModelDefinition
-from ....import_utils import *
+from ....import_utils import ModelDefinition
 from ....models_dict import MODEL_REQUIREMENTS
-from datetime import date
+from ..base import BaseText2Vec
 if is_all_dependency_installed(MODEL_REQUIREMENTS['encoders-text-sentence-transformers']):
     from sentence_transformers import SentenceTransformer
 
-SentenceTransformerModelDefinition = ModelDefinition(markdown_filepath='encoders/text/tfhub/sentence_auto_transformers')
+SentenceTransformerModelDefinition = ModelDefinition(
+    markdown_filepath='encoders/text/sentence_transformers/sentence_auto_transformers'
+)
 
 LIST_OF_URLS = {
     'distilroberta-base-paraphrase-v1' : {"vector_length": 768},
