f03154b04be1d8e373534cd859cdfa3a51ea6008
Jacky <jacky2wong@gmail.com>
2020-11-10 22:38:10 +1100

    Add edge case for text processing

--- utils/upload_cards.py
+++ utils/upload_cards.py
@@ -9,17 +9,18 @@ if __name__=="__main__":
     import os
     import argparse
     import time
+    import re
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--collection_name', default=os.environ['VH_COLLECTION_NAME'])
     args = parser.parse_args()
-    
+
     docs =  get_model_definitions(None)
 
     # Generate 1 sentence summaries for the models
     from transformers import PegasusTokenizer, PegasusForConditionalGeneration
     from typing import List
-    mname = "google/pegasus-xsum"
+    mname = "google/pegasus-large"
 
     model = PegasusForConditionalGeneration.from_pretrained(mname)
     tok = PegasusTokenizer.from_pretrained(mname)
@@ -31,10 +32,22 @@ if __name__=="__main__":
         batch = tok.prepare_seq2seq_batch(src_texts=[text])  # don't need tgt_text for inference
         gen = model.generate(**batch)
         return tok.batch_decode(gen, skip_special_tokens=True)[0]
-    
+
     for i, doc in enumerate(docs):
         short_description = summarise(doc['description'])
         docs[i]['short_description'] = short_description
+        print(short_description)
+
+    def remove_example_from_description(text):
+        # Remove the Example if it is in the middle of the document
+        text = re.sub(r'## Example(.*?)##', '##', text)
+        if '## Example' in text:
+            # Remove if it is at the bottom of the document
+            text = re.sub(r'## Example(.*?)', '', text)
+        return text
+    
+    for i, doc in enumerate(docs):
+        docs[i]['markdown_without_example'] = remove_example_from_description(doc['markdown_description'])
 
     vi_client = ViClient(os.environ['VH_USERNAME'], os.environ['VH_API_KEY'])
     if args.collection_name in vi_client.list_collections():
@@ -46,3 +59,7 @@ if __name__=="__main__":
     print(vi_client.head(args.collection_name))
     print(vi_client.head(args.collection_name)['vector_length'])
     print(vi_client.collection_schema(args.collection_name))
+    import pandas as pd
+    pd.set_option('display.max_colwidth', None)
+    print(vi_client.show_json(vi_client.random_documents(args.collection_name), selected_fields=['markdown_without_example']))
+
