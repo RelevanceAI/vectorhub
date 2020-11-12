50cd9772965c0dc5d1ec3eb0f1fd0efcf0dba059
Jacky Koh <jacky.koh@vylar.org>
2020-11-12 13:44:39 +1100

    remove init test for bert, albert, etc

--- tests/encoders/text/tfhub/test_albert.py
+++ tests/encoders/text/tfhub/test_albert.py
@@ -1,15 +1,6 @@
 from vectorhub.encoders.text.tfhub import Albert2Vec
 import numpy as np
 
-
-def test_albert_initialize():
-    """
-    Testing for albert initialize
-    """
-    client = Albert2Vec()
-    assert True
-
-
 def test_albert_encode():
     """
     Testing for albert initialize
--- tests/encoders/text/tfhub/test_bert.py
+++ tests/encoders/text/tfhub/test_bert.py
@@ -1,19 +1,10 @@
 from vectorhub.encoders.text.tfhub import Bert2Vec
 import numpy as np
 
-
-def test_bert_initialize():
-    """
-    Testing for initialize bert model
-    """
-    client = Bert2Vec()
-    assert True
-
-
 def test_bert_encode():
     """
     Testing for bert encoding
     """
-    client = Bert2Vec()
+    client = Bert2Vec('https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3)
     result = client.encode('Cat')
     assert np.array(result).shape == (1024,)
