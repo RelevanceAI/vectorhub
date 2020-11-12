2266bb2258cfa739dbd331670dea9bcd2a7ffc28
Jacky Koh <jacky.koh@vylar.org>
2020-11-12 13:50:25 +1100

    synatax error on bert test

--- tests/encoders/text/tfhub/test_bert.py
+++ tests/encoders/text/tfhub/test_bert.py
@@ -5,6 +5,6 @@ def test_bert_encode():
     """
     Testing for bert encoding
     """
-    client = Bert2Vec('https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3)
+    client = Bert2Vec('https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/3')
     result = client.encode('Cat')
     assert np.array(result).shape == (1024,)
