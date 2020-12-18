5b56e4453095b61842996cee895306475c1e33fa
Jacky <jacky2wong@gmail.com>
2020-12-18 22:17:19 +1100

    Fix model definition tests

--- tests/test_model_to_dict.py
+++ tests/test_model_to_dict.py
@@ -3,4 +3,4 @@ from vectorhub.auto_encoder import *
 def test_get_model_definitions():
     assert isinstance(get_model_definitions(json_fn=None), list)
     assert isinstance(get_model_definitions(json_fn=None)[0], dict)
-    assert len(get_model_definitions()) > 0
+    assert len(get_model_definitions(json_fn=None)) > 0
