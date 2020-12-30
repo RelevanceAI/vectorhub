97e054212736563014f1bb616866d8ee6e283060
Jacky <jacky2wong@gmail.com>
2020-12-30 20:18:01 +1100

    Add tempclient for testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,35 @@
 import numpy as np
 import random
+import time
 import string
 from vectorhub.utils import list_models, list_installed_models
-from vectorai.tests.utils import TempClient
+from vectorai import ViClient, ViCollectionClient
+
+class TempClient:
+    """Client For a temporary collection
+    """
+    def __init__(self, client, collection_name: str=None):
+        self.client = client
+        if isinstance(client, ViClient):
+            self.collection_name = collection_name
+        elif isinstance(client, ViCollectionClient):
+            self.collection_name = self.client.collection_name
+        
+    def teardown_collection(self):
+        if self.collection_name in self.client.list_collections():
+            time.sleep(2)
+            if isinstance(self.client, ViClient):
+                self.client.delete_collection(self.collection_name)
+            elif isinstance(self.client, ViCollectionClient):
+                self.client.delete_collection()
+    
+    def __enter__(self):
+        self.teardown_collection()
+        return self.client
+    
+    def __exit__(self, *exc):
+        self.teardown_collection()
+
 
 def test_list_models():
     assert len(list_models()) > 0
