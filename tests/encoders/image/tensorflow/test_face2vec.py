473370e226273fe79dcfa043495f23f6f0705351
Jacky <jacky2wong@gmail.com>
2021-01-06 00:24:41 +1100

    Fix vector length for face2vec

--- tests/encoders/image/tensorflow/test_face2vec.py
+++ tests/encoders/image/tensorflow/test_face2vec.py
@@ -10,5 +10,5 @@ def test_face_2_vec_works():
     Testing FaceNet works
     """
     model = Face2Vec()
-    assert_encoder_works(model, 512, data_type='image', 
+    assert_encoder_works(model, 128, data_type='image', 
     image_url='https://www.thestatesman.com/wp-content/uploads/2017/08/1493458748-beauty-face-517.jpg')
