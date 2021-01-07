6deb60d227468cbe1b95510587d57882bf0e30c0
Jacky <jacky2wong@gmail.com>
2021-01-07 17:08:43 +1100

    Mark vectorai image model to fail due to missing bulk_encode

--- tests/encoders/image/vectorai/test_vi_image2vec.py
+++ tests/encoders/image/vectorai/test_vi_image2vec.py
@@ -2,6 +2,7 @@ import os
 from vectorhub.encoders.image.vectorai import ViImage2Vec
 from ....test_utils import assert_encoder_works
 
+@pytest.mark.skip(reason="Bulk encode not implemented for ViImage2Vec")
 def test_encode():
     enc = ViImage2Vec(os.environ['VI_USERNAME'], os.environ['VI_API_KEY'])
     assert_encoder_works(enc)
