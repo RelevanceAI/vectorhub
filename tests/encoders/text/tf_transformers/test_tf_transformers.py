c42caf4f0aa3941813a03017b1d9ac1d7a02b7fb
Jacky <jacky2wong@gmail.com>
2021-01-08 00:21:50 +1100

    Skip a lot of models

--- tests/encoders/text/tf_transformers/test_tf_transformers.py
+++ tests/encoders/text/tf_transformers/test_tf_transformers.py
@@ -5,7 +5,7 @@ import pytest
 from vectorhub.encoders.text.tf_transformers import TFTransformer2Vec
 from ....test_utils import assert_encoder_works 
 
-@pytest.mark.parametrize("model_name",["bert-base-uncased", "distilbert-base-uncased"])
+@pytest.mark.parametrize("model_name",["distilbert-base-uncased"])
 def test_tf_transformer_encode(model_name):
     """
         Test for encoding transformer models
