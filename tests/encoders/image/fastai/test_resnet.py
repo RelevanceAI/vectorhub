dee4b7fab6d2cb713619b4174e091f3013b85a0c
Jacky <jacky2wong@gmail.com>
2021-01-07 16:21:48 +1100

    Fix fastai tests

--- tests/encoders/image/fastai/test_resnet.py
+++ tests/encoders/image/fastai/test_resnet.py
@@ -1,10 +1,9 @@
 """
     Test code for encoding with FastAI.
 """
-
 from vectorhub.encoders.image.fastai import FastAIResnet2Vec
+from ....test_utils import assert_encoder_works
 
 def test_fastai_encoder():
     enc = FastAIResnet2Vec()
-    arr = enc.encode('https://getvectorai.com/assets/logo-square.png')
-    assert len(arr) == 1024
+    assert_encoder_works(enc, 1024, data_type='image')
