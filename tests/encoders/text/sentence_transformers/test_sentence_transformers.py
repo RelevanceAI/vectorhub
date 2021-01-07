3ad8f0ea0249dac378885d5aa5f5ad1401d81372
Jacky <jacky2wong@gmail.com>
2021-01-08 05:26:54 +1100

    Fix test parameters to external pytest

--- tests/encoders/text/sentence_transformers/test_sentence_transformers.py
+++ tests/encoders/text/sentence_transformers/test_sentence_transformers.py
@@ -1,12 +1,13 @@
 import gc
+import pytest
 from vectorhub.encoders.text.sentence_transformers.sentence_auto_transformers import SentenceTransformer2Vec, LIST_OF_URLS
 from ....test_utils import assert_encoder_works
 
-def test_sentence_transformers():
+@pytest.mark.parametrize("model_name", list(LIST_OF_URLS.keys()))
+def test_sentence_transformers(model_name):
     """
         Sentence Transformer
     """
-    for k, v in LIST_OF_URLS.items():
-        enc = SentenceTransformer2Vec(model_name=k)
-        assert_encoder_works(enc, data_type='text')
-        del enc; gc.collect();
+    enc = SentenceTransformer2Vec(model_name=model_name)
+    assert_encoder_works(enc, data_type='text')
+    del enc; gc.collect();
