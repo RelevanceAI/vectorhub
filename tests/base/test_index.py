fde43e1fdeef54720a277836d3e3e1ceecb707dd
Jacky <jacky2wong@gmail.com>
2021-01-12 12:08:35 +1100

    fix index test

--- tests/base/test_index.py
+++ tests/base/test_index.py
@@ -2,34 +2,8 @@
     Tests for various base functions occur here.
 """
 import pytest
-import numpy as np
 import os
-from vectorhub.base import catch_vector_errors, Base2Vec
 from vectorhub.encoders.audio.tfhub import SpeechEmbedding2Vec
-from ..test_utils import is_dummy_vector
-
-def test_catch_vector_errors():
-    """Test the catch vector errors.
-    """
-    encoder =SpeechEmbedding2Vec()
-    vectors = encoder.encode(np.nan)
-    assert is_dummy_vector(vectors)
-
-def test_validate_urls_raises_warning():
-    enc = Base2Vec()
-    with pytest.warns(UserWarning):
-        # Assert this is false
-        assert not enc.validate_model_url('testing_url', ['testing_url_2', 'fake_url'])
-
-def test_validate_urls_works_for_tfhub_exception():
-    enc = Base2Vec()
-    assert enc.validate_model_url('https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/3',
-    ['https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1', 'https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/2'])
-
-def test_validate_urls_works_simple():
-    enc = Base2Vec()
-    assert enc.validate_model_url('test', ['test', 'test_2'])
-
 
 class TestIndex:
     """
@@ -44,7 +18,8 @@ class TestIndex:
             os.environ['VH_API_KEY'],
             items=items,
             collection_name='test_index')
-        assert response['successfully_inserted'] = num_of_documents
+        assert response['successfully_inserted'] == num_of_documents
+        enc.client.delete_collection(enc.collection_name)
 
     def test_vi_index_with_metadata(audio_url):
         """
@@ -60,4 +35,6 @@ class TestIndex:
             items=items,
             metadata=metadata,
             collection_name='test_index')
+        assert response['successfully_inserted'] == num_of_documents
+        enc.client.delete_collection(enc.collection_name)
 
