54742133c1a3d3ec6505aeb38b5b1fa7446f33ed
Jacky <jacky2wong@gmail.com>
2021-01-12 11:13:09 +1100

    Add tighter vectorai integration

---
+++ tests/base/test_index.py
@@ -0,0 +1,63 @@
+"""
+    Tests for various base functions occur here.
+"""
+import pytest
+import numpy as np
+import os
+from vectorhub.base import catch_vector_errors, Base2Vec
+from vectorhub.encoders.audio.tfhub import SpeechEmbedding2Vec
+from ..test_utils import is_dummy_vector
+
+def test_catch_vector_errors():
+    """Test the catch vector errors.
+    """
+    encoder =SpeechEmbedding2Vec()
+    vectors = encoder.encode(np.nan)
+    assert is_dummy_vector(vectors)
+
+def test_validate_urls_raises_warning():
+    enc = Base2Vec()
+    with pytest.warns(UserWarning):
+        # Assert this is false
+        assert not enc.validate_model_url('testing_url', ['testing_url_2', 'fake_url'])
+
+def test_validate_urls_works_for_tfhub_exception():
+    enc = Base2Vec()
+    assert enc.validate_model_url('https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/3',
+    ['https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1', 'https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/2'])
+
+def test_validate_urls_works_simple():
+    enc = Base2Vec()
+    assert enc.validate_model_url('test', ['test', 'test_2'])
+
+
+class TestIndex:
+    """
+    Testing the ability to use and add to the Vector AI index.
+    """
+    def test_vi_index(audio_url):
+        num_of_documents = 30
+        enc = SpeechEmbedding2Vec()
+        items = [audio_url] * num_of_documents
+        response = enc.add_documents(
+            os.environ['VH_USERNAME'],
+            os.environ['VH_API_KEY'],
+            items=items,
+            collection_name='test_index')
+        assert response['successfully_inserted'] = num_of_documents
+
+    def test_vi_index_with_metadata(audio_url):
+        """
+        Test the Vector AI index with Metadata.
+        """
+        num_of_documents = 30
+        enc = SpeechEmbedding2Vec()
+        items= [audio_url] * num_of_documents
+        metadata = list(range(num_of_documents))
+        response = enc.add_documents(
+            os.environ['VH_USERNAME'],
+            os.environ['VH_API_KEY'],
+            items=items,
+            metadata=metadata,
+            collection_name='test_index')
+
---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+"""
+An audio URL.
+"""
+def audio_url():
+    return 'https://vecsearch-bucket.s3.us-east-2.amazonaws.com/voices/common_voice_en_2.wav'
+
