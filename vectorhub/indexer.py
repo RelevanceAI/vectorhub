f4ac2caf67e0e5ed765c8f80f4cf891d05d3bbd9
Jacky <jacky2wong@gmail.com>
2021-01-12 11:54:33 +1100

    Add missing import and clean up

--- vectorhub/indexer.py
+++ vectorhub/indexer.py
@@ -3,10 +3,10 @@ Indexer with the model.
 """
 import warnings
 from vectorai import ViClient, request_api_key
-from typing import List, Any
+from typing import List, Any, Optional
 
 class ViIndexer:
-    def request_api_key(self, username, email, referral=None):
+    def request_api_key(self, username: str, email: str, referral=None):
         """
         Requesting an API key.
         """
@@ -24,6 +24,8 @@ class ViIndexer:
         self.api_key = api_key
         if collection_name is not None:
             self.collection_name = collection_name
+        else:
+            self.collection_name = 'vectorhub_collection_with_' + self.__name__
         if metadata is not None:
             docs = [self._create_document(i, item, metadata) for i, (item, metadata) in enumerate(list(zip(items, metadata)))]
         else:
@@ -32,21 +34,21 @@ class ViIndexer:
         self.client = ViClient(username, api_key)
         return self.client.insert_documents(collection_name, docs, {'item': self})
 
-    def _create_document(self, _id, item, metadata=None):
+    def _create_document(self, _id: str, item: List[str], metadata=None):
         return {
             '_id': str(_id),
             'item': item,
             'metadata': metadata
         }
 
-    def search(self, item, num_results: int=10):
+    def search(self, item: Any, num_results: int=10):
         """
         Simple search with Vector AI
         """
         warnings.warn("If you are looking for more advanced functionality, we recommend using the official Vector AI Github package")
         return self.client.search(collection_name, self.encode(item), field='item_' + self.__name__ + '_vector_', page_size=num_results)
 
-    def retrieve_documents(self, num_of_documents):
+    def retrieve_documents(self, num_of_documents: int):
         """
         Get all the documents in our package.
         """
