23207835ea7e2e0fb5a774a88d5b84425755aede
boba_and_beer <jacky2wong@gmail.com>
2021-01-18 13:31:19 +1100

    [MERGE LAST] Fix package requirements (#50)
    * Fix package requirements
    
    * Update import_utils.py

--- vectorhub/import_utils.py
+++ vectorhub/import_utils.py
@@ -5,42 +5,14 @@ import sys
 import warnings
 import pkg_resources
 import json
+import os
 from importlib import import_module, invalidate_caches
 
 def get_package_requirements(requirement_type: str):
     """
         Load in extra_requirements.json from the package
     """
-    requirements = {
-        "numpy": ["core"],
-        "requests": ["core"],
-        "PyYAML": ["core"],
-        "pytest": ["test"],
-        "sphinx-rtd-theme>=0.5.0": ["test"],
-        "imageio": ["encoders-image", "encoders-image-tfhub"],
-        "scikit-image":  ["encoders-image", "encoders-image-tfhub"],
-        "soundfile": ["encoders-audio-tfhub"],
-        "librosa": ["audio-encoder", "encoders-audio-tfhub"],
-        "tensorflow": ["encoders-text-tfhub", "encoders-audio-tfhub", "encoders-image-tfhub", "encoders-text-tf-transformers", 
-            "encoders-text-tfhub-windows", "encoders-image-tf-face-detection"],
-        "tensorflow_hub": ["encoders-text-tfhub", "encoders-audio-tfhub", "encoders-image-tfhub", "encoders-text-tfhub-windows"],
-        "tensorflow_text": ["encoders-text-tfhub"],
-        "tf-models-official": ["encoders-text-tfhub", "encoders-text-tfhub-windows"],
-        "bert-for-tf2": ["encoders-text-tfhub", "encoders-text-tfhub-windows"],
-        "sentence-transformers": ["encoders-text-sentence-transformers"],
-        "torch>=1.6.0": ["encoders-audio-pytorch", "encoders-text-torch-transformers", "encoders-text-sentence-transformers", 
-            "encoders-image-fastai", "encoders-code-transformers"],
-        "fairseq": ["encoders-audio-pytorch"],
-        "transformers": ["encoders-text-torch-transformers", "encoders-text-tf-transformers", "encoders-code-transformers"],
-        "moviepy": ["encoders-video"],
-        "vectorai": ["test"],
-        "opencv-python": ["encoders-video", "encoders-image-tf-face-detection"],
-        "appdirs": ["encoders-image-tf-face-detection"],
-        "fastai==2.1.8": ["encoders-image-fastai"],
-        "mtcnn": ["encoders-image-tf-face-detection"],
-        "Pillow": ["encoders-image-tf-face-detection"]
-    }
-
+    requirements = json.load(open('extra_requirements.json'))
     dependencies = []
     for k, v in requirements.items():
         if requirement_type in v:
