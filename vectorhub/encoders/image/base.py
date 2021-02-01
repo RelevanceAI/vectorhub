5b47e597fd0cce34be47dad293c07ccb2b2f780b
Jacky Koh <jacky.koh@vylar.org>
2021-02-02 01:43:53 +1100

    add tensorflow image reading

--- vectorhub/encoders/image/base.py
+++ vectorhub/encoders/image/base.py
@@ -21,8 +21,12 @@ class BaseImage2Vec(Base2Vec):
         """
         if type(image) == str:
             if 'http' in image:
-                b = io.BytesIO(urlopen(Request(
-                    quote(image, safe=':/?*=\''), headers={'User-Agent': "Mozilla/5.0"})).read())
+                try:
+                    b = io.BytesIO(urlopen(Request(
+                        quote(image, safe=':/?*=\''), headers={'User-Agent': "Mozilla/5.0"})).read())
+                except:
+                    import tensorflow as tf
+                    return tf.image.decode_jpeg(requests.get(image).content, channels=3, name="jpeg_reader").numpy()
             else:
                 b = image
         elif type(image) == bytes:
