9d9ccc60bd93a42517cababfead1a5f565a7218b
Jacky <jacky2wong@gmail.com>
2021-04-07 23:58:22 +1000

    add is_greyscale conditional

--- vectorhub/encoders/image/base.py
+++ vectorhub/encoders/image/base.py
@@ -38,8 +38,9 @@ class BaseImage2Vec(Base2Vec):
         else:
             raise ValueError("Cannot process data type. Ensure it is is string/bytes or BytesIO.")
         try:
+            if self.is_greyscale:
+                return np.array(imageio.imread(b))
             return np.array(imageio.imread(b, pilmode="RGB"))
-        # TODO: Flesh out exceptions
         except:
             return np.array(imageio.imread(b)[:, :, :3])
 
