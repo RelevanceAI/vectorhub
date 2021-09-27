bc0ebb5ad568f79372da15827f6e19df70f28c6a
boba_and_beer <jacky2wong@gmail.com>
2021-09-27 13:58:58 +1000

    Hotfix/fix clip image preprocessing (#92)
    * Fix CLIP preprocessing
    
    * Force temporary import check

--- vectorhub/bi_encoders/text_image/torch/clip.py
+++ vectorhub/bi_encoders/text_image/torch/clip.py
@@ -9,7 +9,8 @@ from ....import_utils import is_all_dependency_installed
 from ....base import catch_vector_errors
 from ....encoders.image import BaseImage2Vec
 from ....encoders.text import BaseText2Vec
-if is_all_dependency_installed('clip'):
+# if is_all_dependency_installed('clip'):
+try:
     import clip
     import torch
     import numpy as np
@@ -17,6 +18,9 @@ if is_all_dependency_installed('clip'):
     import cv2
     from PIL import Image
     from requests.exceptions import MissingSchema
+except Exception as e:
+    traceback.print_exc()
+    pass
 
 CLIPModelDefinition = ModelDefinition(markdown_filepath='bi_encoders/text_image/torch/clip')
 __doc__ = CLIPModelDefinition.create_docs()
